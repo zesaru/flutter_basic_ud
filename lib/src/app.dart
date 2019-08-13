e355795736a985209b74cb0012704e0090609995
Cesar Murillo <namiayumis@gmail.com>
2019-08-13 16:16:05 +0900

    scaffold-appbar-body

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/src/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
       home: Center(
-        child: Text('Hellos'),
+        child: HomePage(),
       ),
     );
   }
