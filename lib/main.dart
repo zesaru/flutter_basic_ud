ae38086c8440adb1125816b0a8e730605bb3cb44
Cesar Murillo <namiayumis@gmail.com>
2019-08-13 15:48:53 +0900

    new logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/src/app.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      home: Center(
-        child: Text('Hello'),
-      ),
-    );
-  }
 }
